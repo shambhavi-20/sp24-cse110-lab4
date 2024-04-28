d05e84bbe9095f8e94714d622cae1317688a0e63
shambhavi-20 <114725358+shambhavi-20@users.noreply.github.com>
2024-04-27 21:57:49 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,5 +1,7 @@
 let statistics = { redCars:21, blueCars:45, greenCars:12, raceCars:5, blackCars: 40, rareCars:2 };
 
-for (const key in statistics) {
-  console.log('a');
+for (let key in statistics) {
+  if (key[0] == 'r' || statistics[key]%2 != 0){
+    console.log(statistics[key]);
+  }
 }
